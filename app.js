6e445ee5e1ecd30f0225cb6fd04f1fc590bcb23e
mariososadi <mariososadi@gmail.com>
2017-06-06 23:05:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+
+var command =  process.argv[2];
+console.log(process.argv);
+console.log('Command: ', command);
+
+if (command === 'add') {
+
+console.log('Adding new note');
+
+} else if (command === 'list') {
+  console.log('Listing all notes');
+
+
+} else {
+	console.log('Command not recognized');
+
+}
